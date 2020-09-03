1747e9142133028712c88e5d485ec4ed11f883b9
Esteban Gutierrez <48463421+Estebangtz@users.noreply.github.com>
2020-09-02 18:26:09 -0600

    cambios del dia 02092020

---
+++ QAI20_R400_10.a_TA_INSPECCION_MATERIAL_RESULTADO.sql
@@ -0,0 +1,90 @@
+-- //////////////////////////////////////////////////////////////
+-- // ARCHIVO:			
+-- //////////////////////////////////////////////////////////////
+-- // BASE DE DATOS:	[DATA_02Pruebas]
+-- // MODULO:			QA INSPECCION DE MATERIAL
+-- // OPERACION:		LIBERACION / STORED PROCEDURE
+-- //////////////////////////////////////////////////////////////
+-- // Autor:			FEG
+-- // Fecha creación:	02/SEP/2020
+-- ////////////////////////////////////////////////////////////// 
+
+USE [DATA_02Pruebas]
+GO
+
+-- //////////////////////////////////////////////////////////////
+
+
+
+
+
+
+-- //////////////////////////////////////////////////////////////
+-- // DROPs
+-- //////////////////////////////////////////////////////////////
+
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[INSPECCION_MATERIAL_RESULTADO]') AND type in (N'U'))
+	DROP TABLE [dbo].[INSPECCION_MATERIAL_RESULTADO]
+GO
+
+
+
+
+-- //////////////////////////////////////////////////////////////
+-- // INSPECCION_MATERIAL_RESULTADO
+-- //////////////////////////////////////////////////////////////
+
+CREATE TABLE [dbo].[INSPECCION_MATERIAL_RESULTADO] (
+	[K_INSPECCION_MATERIAL_RESULTADO]			[INT]			NOT NULL,
+	-- =================================
+	[K_ORDEN_COMPRA]							VARCHAR(255)	NOT NULL,
+	[NUMERO_PARTE]								VARCHAR(150)	NOT NULL,	
+	[K_INSPECCION_MATERIAL]						[INT]			NOT NULL,
+	[OPCION_SELECCIONADA]						VARCHAR(255)	NOT NULL,
+	[OPCION_PORCENTAJE]							DECIMAL(13,2)	NOT NULL,
+	-- =================================	
+	[F_INSPECCION_MATERIAL_RESULTADO]			DATE			NOT NULL
+)ON [PRIMARY]	
+GO
+
+-- //////////////////////////////////////////////////////
+
+ALTER TABLE [dbo].[INSPECCION_MATERIAL_RESULTADO]
+	ADD CONSTRAINT [PK_INSPECCION_MATERIAL_RESULTADO]
+		PRIMARY KEY CLUSTERED ([K_INSPECCION_MATERIAL_RESULTADO])
+GO
+
+-- //////////////////////////////////////////////////////////////
+
+
+--ALTER TABLE [dbo].[INSPECCION_MATERIAL_RESULTADO] ADD 
+	--CONSTRAINT [FK_INSPECCION_MATERIAL_RESULTADO_01]  
+	--	FOREIGN KEY ([K_PUESTO_DESCRIPCION]) 
+	--	REFERENCES [dbo].[PUESTO_DESCRIPCION] ([K_PUESTO_DESCRIPCION]),
+	--CONSTRAINT [FK_INSPECCION_MATERIAL_RESULTADO_02]  
+	--	FOREIGN KEY ([K_TIPO_INSPECCION_MATERIAL_RESULTADO]) 
+	--	REFERENCES [dbo].[TIPO_INSPECCION_MATERIAL_RESULTADO] ([K_TIPO_INSPECCION_MATERIAL_RESULTADO]),
+	--CONSTRAINT [FK_INSPECCION_MATERIAL_RESULTADO_03]  
+	--	FOREIGN KEY ([K_ESTATUS_INSPECCION_MATERIAL_RESULTADO]) 
+	--	REFERENCES [dbo].[ESTATUS_INSPECCION_MATERIAL_RESULTADO] ([K_ESTATUS_INSPECCION_MATERIAL_RESULTADO])
+--GO
+
+
+-- //////////////////////////////////////////////////////
+
+
+ALTER TABLE [dbo].[INSPECCION_MATERIAL_RESULTADO] 
+	ADD		[K_USUARIO_ALTA]				[INT]		NOT NULL,
+			[F_ALTA]						[DATETIME]	NOT NULL,
+			[K_USUARIO_CAMBIO]				[INT]		NOT NULL,
+			[F_CAMBIO]						[DATETIME]	NOT NULL,
+			[L_BORRADO]						[INT]		NOT NULL,
+			[K_USUARIO_BAJA]				[INT]		NULL,
+			[F_BAJA]						[DATETIME]	NULL;
+GO
+
+
+
+-- //////////////////////////////////////////////////////////////
+-- //////////////////////////////////////////////////////////////
+-- //////////////////////////////////////////////////////////////
