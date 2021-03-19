6a80b9aca7c832a5324049a85fe2791663af3f1f
Estebangtz <48463421+Estebangtz@users.noreply.github.com>
2021-03-19 17:15:38 -0600

    cambios del dia 19032021

--- INSPECCION_MATERIAL/INSPECCION_PIEL/TEST.sql
+++ INSPECCION_MATERIAL/INSPECCION_PIEL/TEST.sql
@@ -9,17 +9,6 @@ SELECT DISTINCT [STATUS] FROM	IncInsp_sql
 select * from PPMS_PEARL.DBO.def where descripcion='NATURAL'
 
 
-SELECT TOP 10 * FROM Perforacion WHERE NOSERIE = '25984005'
-
-
-SELECT TOP 100 * FROM cccuthst_sql WHERE --machine = 'Table 37' AND cdate = 20210316 AND 
-jobno = '26220 '
-
-
---UPDATE cccuthst_sql 
---SET cshift = 0
---WHERE jobno = '26220'
-
 
 
 
